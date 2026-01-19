9c27b2bf4ee6856e27e8c08a2181915204dc96ee
ChilledEther <jarredroberts@outlook.com>
2026-01-19 12:22:13 +0000

    feat(01-03): implement main generator entrypoint

--- pkg/scaffold/project.go
+++ pkg/scaffold/project.go
@@ -1,5 +1,7 @@
 package scaffold
 
+import "fmt"
+
 // ProjectGenerator orchestrates the generation of the entire project
 type ProjectGenerator struct {
 	Actions Generator
@@ -20,6 +22,35 @@ func NewProjectGenerator() *ProjectGenerator {
 var _ Generator = (*ProjectGenerator)(nil)
 
 func (g *ProjectGenerator) Generate(cfg Config) ([]File, error) {
-	// TODO: Implement wiring
-	return nil, nil
+	if err := cfg.Validate(); err != nil {
+		return nil, err
+	}
+
+	var allFiles []File
+
+	if cfg.WithActions {
+		files, err := g.Actions.Generate(cfg)
+		if err != nil {
+			return nil, fmt.Errorf("actions generation failed: %w", err)
+		}
+		allFiles = append(allFiles, files...)
+	}
+
+	if cfg.WithDocker {
+		files, err := g.Docker.Generate(cfg)
+		if err != nil {
+			return nil, fmt.Errorf("docker generation failed: %w", err)
+		}
+		allFiles = append(allFiles, files...)
+	}
+
+	if cfg.WithFlux {
+		files, err := g.Flux.Generate(cfg)
+		if err != nil {
+			return nil, fmt.Errorf("flux generation failed: %w", err)
+		}
+		allFiles = append(allFiles, files...)
+	}
+
+	return allFiles, nil
 }
